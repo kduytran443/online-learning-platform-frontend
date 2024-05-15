99bb7a75b8ebf52a4a05d58a3afa8e9572240643
kduytran443 <trankhanhduy18@gmail.com>
2024-05-16 03:58:41 +0700

    #client-main: main layout header

--- online-learning-platform-frontend/src/components/NavBarHeader.vue
+++ online-learning-platform-frontend/src/components/NavBarHeader.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref } from "vue";
+import { mergeProps, ref } from "vue";
 
 const { appTitle, menuItems } = {
   appTitle: import.meta.env.VITE_APP_NAME,
@@ -9,6 +9,19 @@ const { appTitle, menuItems } = {
     { title: "Sign In", path: "/signin", icon: "mdi-lock" }
   ]
 };
+
+const items = ref([
+  { code: "code", name: "Development" },
+  { code: "code", name: "Business" },
+  { code: "code", name: "Finance & Accounting" },
+  { code: "code", name: "IT & Software" },
+  { code: "code", name: "Office Productivity" },
+  { code: "code", name: "Personal Development" },
+  { code: "code", name: "Design" },
+  { code: "code", name: "Marketing" },
+  { code: "code", name: "Health & Fitness" },
+  { code: "code", name: "Music" }
+]);
 const sidebar = ref(false);
 </script>
 
@@ -45,6 +58,26 @@ const sidebar = ref(false);
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <v-toolbar-items class="tw-hidden sm:tw-block">
+        <v-menu>
+          <template v-slot:activator="{ props: menu }">
+            <v-tooltip location="top">
+              <template v-slot:activator="{ props: tooltip }">
+                <v-btn v-bind="mergeProps(menu, tooltip)"
+                  ><v-icon left dark>mdi-shape</v-icon>
+                  <div class="tw-ml-2">Category</div>
+                </v-btn>
+              </template>
+              <span>Show all categories</span>
+            </v-tooltip>
+          </template>
+          <v-list>
+            <v-list-item v-for="(item, index) in items" :key="index" :to="`/category/${item.code}`">
+              <v-list-item-action>
+                <v-list-item-title>{{ item.name }}</v-list-item-title>
+              </v-list-item-action>
+            </v-list-item>
+          </v-list>
+        </v-menu>
         <v-btn v-for="item in menuItems" :key="item.title" :to="item.path">
           <v-icon left dark>{{ item.icon }}</v-icon>
           <div class="tw-ml-2">{{ item.title }}</div>
