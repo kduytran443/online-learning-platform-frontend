ade2471a8888fe67d0915cb519abda1f68b454b9
kduytran443 <trankhanhduy18@gmail.com>
2024-05-16 03:40:01 +0700

    #client-main: v-icon

--- online-learning-platform-frontend/src/main.ts
+++ online-learning-platform-frontend/src/main.ts
@@ -8,10 +8,16 @@ import "vuetify/styles";
 import { createVuetify } from "vuetify";
 import * as components from "vuetify/components";
 import * as directives from "vuetify/directives";
+import { aliases, mdi } from "vuetify/iconsets/mdi";
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
+import "vuetify/styles";
+import "@mdi/font/css/materialdesignicons.css";
 
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+
 import assignGlobalComponents from "./utils/importComponents";
 
 const app = createApp(App);
@@ -21,7 +27,15 @@ app.use(router);
 
 assignGlobalComponents(app);
 
+const icons = {
+  defaultSet: "mdi",
+  aliases,
+  sets: {
+    mdi
+  }
+};
 const vuetify = createVuetify({
+  icons,
   components,
   directives
 });
