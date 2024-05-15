8aac1513222d8683736348490f450286384b7ae9
kduytran443 <trankhanhduy18@gmail.com>
2024-05-16 00:54:30 +0700

    #client-main: Make main layout

---
+++ online-learning-platform-frontend/src/layouts/MainLayout/MainLayout.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import MainLayoutHeader from "./MainLayoutHeader.vue";
+import MainLayoutFooter from "./MainLayoutFooter.vue";
+</script>
+
+<template>
+  <MainLayoutHeader />
+  <main>
+    <slot />
+  </main>
+  <MainLayoutFooter />
+</template>
