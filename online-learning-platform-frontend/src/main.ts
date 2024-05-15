c7efdffa11b86989b7f4dd3ea2a0dc670aaa96f3
kduytran443 <trankhanhduy18@gmail.com>
2024-05-16 00:51:45 +0700

    #client-main: Make assign components

--- online-learning-platform-frontend/src/main.ts
+++ online-learning-platform-frontend/src/main.ts
@@ -6,10 +6,13 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 import "./index.css";
+import assignGlobalComponents from "./utils/importComponents";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+
+assignGlobalComponents(app);
 
 app.mount("#app");
