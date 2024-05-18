837ccc037501fa9afd2984a3b03489495c59d04e
kduytran443 <trankhanhduy18@gmail.com>
2024-05-18 11:36:14 +0700

    #client-main: Make url constants

---
+++ online-learning-platform-frontend/src/constants/urlConstant.ts
@@ -0,0 +1,9 @@
+const LOGIN_URL = "/log-in";
+const SIGN_UP = "/sign-up";
+const RESEND_REGISTRATION_CONFIRM = "/resend-registration-confrm";
+
+export default {
+  LOGIN_URL,
+  SIGN_UP,
+  RESEND_REGISTRATION_CONFIRM
+};
