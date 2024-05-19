db47cbf0e1de7548cf5eb30c07c9074c9d204055
kduytran443 <trankhanhduy18@gmail.com>
2024-05-19 21:04:14 +0700

    #client-main: Login page

--- online-learning-platform-frontend/src/router/index.ts
+++ online-learning-platform-frontend/src/router/index.ts
@@ -2,6 +2,9 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import SignUpView from "../views/SignUpView.vue";
 import urlConstant from "@/constants/urlConstant";
+import ResendMailConfirmView from "@/views/ResendMailConfirmView.vue";
+import LoginView from "@/views/LoginView.vue";
+import CategoryView from "@/views/CategoryView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,9 +31,27 @@ const router = createRouter({
     },
     {
       path: urlConstant.SIGN_UP,
-      name: "sign up",
+      name: "sign-up",
       meta: { layout: "main-layout" },
       component: SignUpView
+    },
+    {
+      path: urlConstant.RESEND_REGISTRATION_CONFIRM,
+      name: "resend-registration-confrm",
+      meta: { layout: "main-layout" },
+      component: ResendMailConfirmView
+    },
+    {
+      path: urlConstant.LOGIN_URL,
+      name: "login",
+      meta: { layout: "main-layout" },
+      component: LoginView
+    },
+    {
+      path: urlConstant.CATEGORY_URL,
+      name: "category",
+      meta: { layout: "main-layout" },
+      component: CategoryView
     }
   ]
 });
