2b2d94b7839f3e2c2c9d9038f06fb6c40a6c7d0f
kduytran443 <trankhanhduy18@gmail.com>
2024-05-19 21:01:35 +0700

    #client-main: Make class card

---
+++ online-learning-platform-frontend/src/components/SimpleAvatarShow.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="tw-w-full tw-flex tw-items-center">
+    <v-avatar>
+      <v-img alt="John" src="https://cdn.vuetifyjs.com/images/john.jpg"></v-img>
+    </v-avatar>
+    <div class="pl-4">John</div>
+  </div>
+</template>
