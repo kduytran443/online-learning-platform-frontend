7a5d472fea4902a85abe3b62f4db3dccaa306aff
kduytran443 <trankhanhduy18@gmail.com>
2025-02-17 00:18:45 +0700

    #react-main: using ApiFetcher

--- olp-frontend-app/src/views/home/HomeView.tsx
+++ olp-frontend-app/src/views/home/HomeView.tsx
@@ -1,14 +1,19 @@
 import { Box } from "@mui/material";
-import Tippy from "@tippyjs/react";
+import ApiFetcher from "api/ApiFetcher";
+import { getSimpleClasses } from "api/class/ClassApi";
+import ClassGallery from "components/ClassGallery";
 
 export function HomeView() {
-    return <Box
-        sx={{ display: 'flex' }}
-    >
-    <Box component="main" sx={{ flexGrow: 1, p: 4 }}>
-      <Tippy content="Hello">
-        <p>Dashboard content for Homeview</p>
-      </Tippy>
+  return (
+    <Box sx={{ display: "flex" }}>
+      <Box component="main" sx={{ flexGrow: 1, p: 4 }}>
+          <>
+            <p>Dashboard content for Homeview</p>
+            <ApiFetcher fetchData={ getSimpleClasses }>
+              {(itemList, loading, error) => <ClassGallery items={itemList} error={error} loading={loading} />}
+            </ApiFetcher>
+          </>
+      </Box>
     </Box>
-  </Box>;
+  );
 }
